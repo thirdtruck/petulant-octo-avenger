40feb113475660c5842fe0a3891fcf4d89be9a48
James C. Holder <jholder@theladders.com>
2013-09-04 17:23:09 -0400

    Crop the uploaded image before previewing it.

--- cropper.js
+++ cropper.js
@@ -2,9 +2,25 @@ $(document).ready(function() {
   var $uploadImage = $('#uploaded-image');
   var $previewImage = $('#preview-image');
 
+  function cropByHalf(imageSource) {
+    var tempImage = new Image();
+    tempImage.src = imageSource;
+
+    var canvasWidth = 200;
+    var canvasHeight = 200;
+    var $canvas = $('<canvas width="'+ canvasWidth +'" height="'+ canvasHeight +'"/>');
+
+    var ctx = $canvas.get(0).getContext('2d');
+    ctx.drawImage(tempImage, 0, 0);
+    var croppedSource = $canvas.get(0).toDataURL();
+
+    return croppedSource;
+  }
+
   function displayLoadedImage(el) {
     imageSource = el.target.result;
-    $previewImage.attr('src', imageSource);
+    croppedSource = cropByHalf(imageSource);
+    $previewImage.attr('src', croppedSource);
   }
 
   $uploadImage.on('change', function() {
