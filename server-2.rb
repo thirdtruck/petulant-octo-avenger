193904e34dce9aca6becdbb96825d36a41d00358
James C. Holder <jholder@theladders.com>
2013-09-09 16:57:01 -0400

    Add upload-focused example server and support files and directories.

---
+++ server-2.rb
@@ -0,0 +1,20 @@
+#!/usr/bin/env ruby
+#http://www.wooptoot.com/file-upload-with-sinatra
+
+require 'rubygems'
+require 'sinatra'
+require 'haml'
+ 
+# Handle GET-request (Show the upload form)
+get "/upload" do
+  haml :upload
+end      
+    
+# Handle POST-request (Receive and save the uploaded file)
+post "/upload" do 
+  File.open('uploads/' + params['myfile'][:filename], "w") do |f|
+    f.write(params['myfile'][:tempfile].read)
+  end
+  return "The file was successfully uploaded!"
+end
+
