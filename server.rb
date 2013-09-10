06abbedfa8c931a44580c7e951c2599fee55f293
James C. Holder <jholder@theladders.com>
2013-09-10 11:46:49 -0400

    Fix spelling and HTML formatting errors.

--- server.rb
+++ server.rb
@@ -51,18 +51,18 @@ end
 get '/uploaded_memory' do
   image_in_memory
   data_uri = Base64.encode64(image_in_memory).gsub(/\n/, '')
-  image_tag = "<img src='data:image/jpeg;base64,#{data_uri} />"
-  iamge_tag
+  image_tag = "<img src='data:image/jpeg;base64,#{data_uri}' />"
+  image_tag
 end
 
 get '/uploaded_memory_jpg' do
   data_uri = Base64.encode64(image_in_memory).gsub(/\n/, '')
-  image_tag = "<img src='data:image/jpeg;base64,#{data_uri} />"
-  iamge_tag
+  image_tag = "<img src='data:image/jpeg;base64,#{data_uri}' />"
+  image_tag
 end
 
 get '/uploaded_memory_png' do
   data_uri = Base64.encode64(image_in_memory).gsub(/\n/, '')
-  image_tag = "<img src='data:image/png;base64,#{data_uri} />"
-  iamge_tag
+  image_tag = "<img src='data:image/png;base64,#{data_uri}' />"
+  image_tag
 end
