226e15fc07b57bde87452692e23c2004789a9416
James C. Holder <jholder@theladders.com>
2013-09-10 16:31:22 -0400

    When loading in IE8-9, replace the default file upload form with a fallback one.

--- cropper.js
+++ cropper.js
@@ -1,5 +1,3 @@
-var triggerOldUpload;
-
 $(document).ready(function() {
   var previewImageID = 'preview-image';
 
@@ -13,6 +11,8 @@ $(document).ready(function() {
     h: 200
   };
 
+  var $uploadForm = $('#uploaded-form');
+
   var $uploadImage = $('#uploaded-image');
   var $previewImage = $('#'+previewImageID);
   var $previewImageLink = $('#preview-image-link');
@@ -20,81 +20,90 @@ $(document).ready(function() {
   var $onlyAfterUpload = $('.only-after-upload');
   var $onlyAfterCrop = $('.only-after-crop');
 
-  function updatePreview(imageSource, cropBox) {
-    var ctx = $previewCropped.get(0).getContext('2d');
+  function newUpload() {
+    function updatePreview(imageSource, cropBox) {
+      var ctx = $previewCropped.get(0).getContext('2d');
 
-    var tempImage = new Image();
-    tempImage.src = imageSource;
+      var tempImage = new Image();
+      tempImage.src = imageSource;
 
-    $previewCropped.attr('width', cropBox.w);
-    $previewCropped.attr('height', cropBox.h);
+      $previewCropped.attr('width', cropBox.w);
+      $previewCropped.attr('height', cropBox.h);
 
-    ctx.drawImage(tempImage, cropBox.x, cropBox.y, cropBox.w, cropBox.h, 0, 0, cropBox.w, cropBox.h);
-  }
-
-  function displayLoadedImage(el) {
-    imageSource = el.target.result;
-
-    $onlyAfterUpload.show();
-    $onlyAfterCrop.hide();
-
-    $oldImage = $previewImage;
-    $previewImage = $('<img id="'+previewImageID+'" />')
-    if(jcrop_api) {
-      jcrop_api.destroy();
+      ctx.drawImage(tempImage, cropBox.x, cropBox.y, cropBox.w, cropBox.h, 0, 0, cropBox.w, cropBox.h);
     }
-    $previewImage.replaceAll($oldImage);
 
-    $previewImage.attr('src', imageSource);
+    function displayLoadedImage(el) {
+      imageSource = el.target.result;
 
-    $previewImageLink.attr('href', imageSource);
-    $previewImageLink.text('Download cropped image');
+      $onlyAfterUpload.show();
+      $onlyAfterCrop.hide();
 
-    $previewImage.Jcrop(
-      {
-        aspectRatio: 1,
-        setSelect: [defaultCropBox.x, defaultCropBox.y, defaultCropBox.w, defaultCropBox.h],
-        onSelect: function(cropBox) {
-          $onlyAfterCrop.show();
-          updatePreview(imageSource, cropBox);
-        }
+      $oldImage = $previewImage;
+      $previewImage = $('<img id="'+previewImageID+'" />')
+      if(jcrop_api) {
+        jcrop_api.destroy();
+      }
+      $previewImage.replaceAll($oldImage);
 
-      },
-      function() { jcrop_api = this; }
-    );
-    updatePreview(imageSource, defaultCropBox);
-  }
+      $previewImage.attr('src', imageSource);
 
-  var iFrameIdentifier = 'upload-iframe';
+      $previewImageLink.attr('href', imageSource);
+      $previewImageLink.text('Download cropped image');
 
-  function buildUploadiFrame() {
-    var $iframe = $('<iframe />');
-    $iframe.attr('id', iFrameIdentifier);
-    $iframe.attr('name', iFrameIdentifier);
-    $iframe.css('display', 'none');
-    var antilock = function() {
-      var running = false;
+      $previewImage.Jcrop(
+        {
+          aspectRatio: 1,
+          setSelect: [defaultCropBox.x, defaultCropBox.y, defaultCropBox.w, defaultCropBox.h],
+          onSelect: function(cropBox) {
+            $onlyAfterCrop.show();
+            updatePreview(imageSource, cropBox);
+          }
+
+        },
+        function() { jcrop_api = this; }
+      );
+      updatePreview(imageSource, defaultCropBox);
     }
-    $iframe.on('error', antilock);
-    $iframe.ready(antilock);
 
-    return $iframe;
-  }
+    var iFrameIdentifier = 'upload-iframe';
 
-  function buildUploadForm() {
-    var $form = $('<form />');
-    $form.attr('id', 'iframe-upload');
-    $form.attr('method', 'POST');
-    $form.attr('action', '/upload_memory');
-    $form.attr('target', iFrameIdentifier);
-    $form.attr('encoding', 'multipart/form-data');
-    $form.attr('enctype', 'multipart/form-data');
-    var $clone = $uploadImage.clone();
-    var $submit = $('<input type="submit" />');
-    $form.append($clone);
-    $form.append($submit);
+    function buildUploadiFrame() {
+      var $iframe = $('<iframe />');
+      $iframe.attr('id', iFrameIdentifier);
+      $iframe.attr('name', iFrameIdentifier);
+      $iframe.css('display', 'none');
+      var antilock = function() {
+        var running = false;
+      }
+      $iframe.on('error', antilock);
+      $iframe.ready(antilock);
 
-    return $form;
+      return $iframe;
+    }
+
+    function buildUploadForm() {
+      var $form = $('<form />');
+      $form.attr('id', 'iframe-upload');
+      $form.attr('method', 'POST');
+      $form.attr('action', '/upload_memory');
+      $form.attr('target', iFrameIdentifier);
+      $form.attr('encoding', 'multipart/form-data');
+      $form.attr('enctype', 'multipart/form-data');
+      var $clone = $uploadImage.clone();
+      var $submit = $('<input type="submit" />');
+      $form.append($clone);
+      $form.append($submit);
+
+      return $form;
+    }
+
+    $uploadImage.on('change', function() {
+      var file = this.files[0];
+      var reader = new FileReader();
+      $(reader).on('load', displayLoadedImage);
+      reader.readAsDataURL(file);
+    });
   }
 
   function oldUpload() {
@@ -102,7 +111,7 @@ $(document).ready(function() {
     $('body').append($iframe);
 
     var $form = buildUploadForm();
-    $('body').append($form);
+    $uploadForm.replaceWith($form);
 
     $form.submit(function() {
       $.get('/uploaded_memory_jpg', function(image_tag) {
@@ -111,16 +120,9 @@ $(document).ready(function() {
     });
   }
 
-  $uploadImage.on('change', function() {
-    if(typeof FileReader !== "undefined") {
-      var file = this.files[0];
-      var reader = new FileReader();
-      $(reader).on('load', displayLoadedImage);
-      reader.readAsDataURL(file);
-    } else {
-      oldUpload();
-    }
-  });
-
-  triggerOldUpload = function() { oldUpload(); }
+  if(typeof FileReader !== "undefined") {
+    newUpload();
+  } else {
+    oldUpload();
+  }
 });
