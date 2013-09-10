1d8cc94951e89f801532f6034d5768e3a5635257
James C. Holder <jholder@theladders.com>
2013-09-10 09:45:34 -0400

    Add preliminary upload feature.
    Add redirect for root URL.

--- server.rb
+++ server.rb
@@ -11,6 +11,10 @@ image_index = 1
 
 set :public_folder, '.'
 
+get '/' do
+  redirect '/index.html'
+end
+
 post '/upload_image' do
   data = request['image_data']
   image_data_url = data
@@ -26,3 +30,12 @@ end
 get '/uploaded_image_src' do
   image_data_url || 'data:image/png;base64,'
 end
+
+# Handle POST-request (Receive and save the uploaded file)
+post "/upload" do 
+  File.open('uploads/' + params['myfile'][:filename], "w") do |file|
+    file.write(params['myfile'][:tempfile].read)
+  end
+  redirect '/'
+end
+
